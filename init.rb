14fa363076fda455a2609d6736c460322dadabca
Eric Davis <edavis@littlestreamsoftware.com>
2015-03-25 14:44:34 -0700

    Generated plugin

---
+++ init.rb
@@ -0,0 +1,7 @@
+Redmine::Plugin.register :redmine_power_subtasks do
+  name 'Redmine Power Subtasks plugin'
+  author 'Eric Davis'
+  description "Customizations to Redmine's subtasks to make them more powerful."
+  version '1.0.0'
+  author_url 'http://www.littlestreamsoftware.com'
+end
